5f4dee9cc80e330952e3f98036be85c2db86d02e
aplygs <a4xt3s@gmail.com>
2025-09-14 12:43:53 +0300

    task 4 shag 2 compl

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-const dlg = document.getElementById('contactDialog');
+const dlg = document.getElementById('contactDialog'); //мы ищем элемент html с id contdialog и сохраняем в переменную dlg
 const openBtn = document.getElementById('openDialog');
 const closeBtn = document.getElementById('closeDialog');
 const form = document.getElementById('contactForm');
-let lastActive = null;
+let lastActive = null; //создаем переменную но пока что не вписываем туда ничего, она будет запоминать что было открыто перед тем как пользователь открыл окно диалога
 
-openBtn.addEventListener('click', () => {
-  lastActive = document.activeElement;
-  dlg.showModal(); // модальный режим + затемнение
-  dlg.querySelector('input,select,textarea,button')?.focus();
+openBtn.addEventListener('click', () => { //когда по кнопке кликнут запусти функцию ниже, openBtn это переменная с нашей кнопкой
+  lastActive = document.activeElement; //activeElemnt свойство которое показывает какой элемент на странице сейчас активен. мы сохраняем его.
+  dlg.showModal(); //открываем диалог в модальном окне, это метод для dlg
+  dlg.querySelector('input,select,textarea,button')?.focus(); //Найди в диалоге первое поле ввода, список или кнопку, и если нашёл — поставь туда курсор. Если не нашёл — ничего страшного, просто пропусти эту команду
 });
 
-closeBtn.addEventListener('click', () => dlg.close('cancel'));
+closeBtn.addEventListener('click', () => dlg.close('cancel')); //когда кликаем по closeBtn вызывается dlg close с причиной cancel
 
-form?.addEventListener('submit', (e) => {
+form?.addEventListener('submit', (e) => { //если форма существует (а она сущетсвует, в html разметке), то при ее отправки выполни эту функцию, передав информацию о событии
   // 1) Сброс кастомных сообщений
   [...form.elements].forEach(el => el.setCustomValidity?.(''));
 
